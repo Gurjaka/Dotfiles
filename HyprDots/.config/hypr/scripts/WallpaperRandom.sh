d48793dd0aaed052aa2c377625edf26d6add18e5
Gurjaka <esartia.gurika@gmail.com>
2024-03-03 21:29:52 +0400

    HyprDots

---
+++ HyprDots/.config/hypr/scripts/WallpaperRandom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+wallDIR="/home/gurami/Pictures/wallpapers"
+scriptsDir="/home/gurami/.config/hypr/scripts"
+
+PICS=($(find ${wallDIR} -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+
+# Transition config
+FPS=60
+TYPE="grow"
+DURATION=1
+BEZIER=".43,1.19,1,.4"
+SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-pos top-right --transition-duration $DURATION --transition-bezier $BEZIER"
+
+swww query || swww init && swww img ${RANDOMPICS} $SWWW_PARAMS
+
