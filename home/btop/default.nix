e0304099a576f7bc90bebaca29a22f935e125d1f
Gurjaka <esartia.gurika@gmail.com>
2024-06-21 22:45:28 +0400

    btop fix

--- home/btop/default.nix
+++ home/btop/default.nix
@@ -2,10 +2,8 @@
 {
   programs.btop = {
     enable = true;
-    package = pkgs.btop;
     settings = {
-        color-theme = "nord";
-        theme_background = true;
+        color_theme = "nord";
     };
   };
 }
