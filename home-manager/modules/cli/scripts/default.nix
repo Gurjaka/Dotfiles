0e840ed06220fb3044fecf346c1ef49e5d012208
Gurjaka <esartia.gurika@gmail.com>
2025-02-25 23:03:34 +0400

    Improve scripts management + coding tmux layout script

--- home-manager/modules/cli/scripts/default.nix
+++ home-manager/modules/cli/scripts/default.nix
@@ -1,13 +1,26 @@
 {pkgs, ...}: {
   home.packages = let
-    pyflake = import ./pyflake.nix {inherit pkgs;};
-    pyshell = import ./pyshell.nix {inherit pkgs;};
-    c-learning = import ./c-learning.nix {inherit pkgs;};
-    pull_wallpapers = import ./pull_wallpapers.nix {inherit pkgs;};
-  in [
-    pyflake
-    pyshell
-    c-learning
-    pull_wallpapers
-  ];
+    # Get the directory containing this file
+    dir = ./.;
+
+    # List all files in the directory
+    files = builtins.attrNames (builtins.readDir dir);
+
+    # Filter to only include .nix files and exclude this file
+    nixFiles =
+      builtins.filter (
+        name:
+          builtins.match ".*\\.nix" name
+          != null
+          && name != "default.nix" # Assuming this is default.nix
+      )
+      files;
+
+    # Import each .nix file with pkgs
+    importFile = file: import (dir + "/${file}") {inherit pkgs;};
+
+    # Map the import function over all filtered files
+    packages = builtins.map importFile nixFiles;
+  in
+    packages;
 }
