65e512a78c868b7392f5827036d9463ba14a7b83
Gurjaka <esartia.gurika@gmail.com>
2025-04-13 17:54:37 +0400

    fix spotify theme

--- home-manager/modules/desktop/spotify.nix
+++ home-manager/modules/desktop/spotify.nix
@@ -6,15 +6,16 @@
   programs.spicetify = let
     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
   in {
-    enable = false;
+    enable = true;
     enabledExtensions = with spicePkgs.extensions; [
       adblock
+      autoSkipVideo
       hidePodcasts
       shuffle
       fullScreen
       volumePercentage
     ];
     theme = spicePkgs.themes.comfy;
-    colorScheme = "nord";
+    colorScheme = "Nord";
   };
 }
