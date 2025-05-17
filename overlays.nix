cca1ceb053236361ccabe5cbb1facf3bfe27da1f
Gurjaka <esartia.gurika@gmail.com>
2025-05-17 18:33:16 +0400

    update: remove qtile-extras tests overlay

--- overlays.nix
+++ overlays.nix
@@ -12,14 +12,6 @@
           (python-final: python-prev: {
             qtile-extras = python-prev.qtile-extras.overridePythonAttrs (oldAttrs: {
               src = inputs.qtile-extras-flake.outPath;
-              # disable broken tests
-              disabledTestPaths =
-                oldAttrs.disabledTestPaths
-                ++ [
-                  "test/widget/test_animated_image.py"
-                  "test/widget/test_groupbox2.py"
-                  "test/widget/test_image.py"
-                ];
             });
           })
         ];
