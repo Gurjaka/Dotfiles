d712fe3cf824517f5330d162d605c808bb66a926
Gurjaka <esartia.gurika@gmail.com>
2025-06-08 16:02:30 +0400

    update: add obs-studio for streaming

--- home-manager/modules/desktop/obs.nix
+++ home-manager/modules/desktop/obs.nix
@@ -1,6 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  host,
+  ...
+}: {
   programs.obs-studio = {
-    enable = true;
+    enable =
+      if host == "desktop"
+      then true
+      else false;
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs
       droidcam-obs
